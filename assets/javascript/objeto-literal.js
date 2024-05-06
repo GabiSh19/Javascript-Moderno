e746575c278ce1762c5460317737f005ff84ad60
Gabi13st.h <gabs13.h@gmail.com>
2024-05-05 21:53:01 -0400

    Añade comentarios sobre los objetos

--- assets/javascript/objeto-literal.js
+++ assets/javascript/objeto-literal.js
@@ -16,6 +16,8 @@ const personaje = {
 };
 
 console.log( personaje );
+
+// Para acceder a las propiedades del objeto personaje podemos hacerlo de la siguiente manera:
 console.log('Nombre', personaje.nombre );
 console.log('Nombre', personaje['nombre'] );
 console.log('Edad', personaje.edad );
@@ -24,6 +26,7 @@ console.log('Coors', personaje.coords );
 console.log('Lat', personaje.coords.lat );
 
 console.log('No. Trajes', personaje.trajes.length );
+//Cómo obtener el último traje del personae:
 console.log('último traje', personaje.trajes[ personaje.trajes.length - 1 ] );
 
 const x = 'vivo';
@@ -33,26 +36,28 @@ console.log('Última película', personaje['ultima-pelicula'] );
 
 // Más detalles
 
+//Para eliminar una propiedad usa DELETE
 delete personaje.edad;
 console.log( personaje );
 
+// Para agregar una propiedad asignale un valor
 personaje.casado =  true;
 
-
+// Para trabajar el objeto como arreglo usa OBJECT.ENTRIES
 const entriesPares = Object.entries( personaje );
 console.log( entriesPares );
 
-// personaje = true;
-
+// Para bloquear el objeto, es decir, que no se agregen o modifiquen propiedades usa OBJECT.FREEZE
 Object.freeze( personaje );
-
 personaje.dinero = 10000000000;
 personaje.casado = false;
+//Observación: en caso que haya otro objeto dentro, si se pueden modificar estos valores. A menos que le hagas un freeze.
 personaje.direccion.ubicacion = 'Costa Rica';
 console.log( personaje );
 
-
+//Para obtener las propiedades que tiene el objeto usa OBJECT.GETOWNPROPERTYNAMES
 const propiedades = Object.getOwnPropertyNames( personaje );
+//Para obtener los valores de las propiedades usa OBJECT.values
 const valores     = Object.values( personaje );
 console.log({ propiedades, valores });
 
