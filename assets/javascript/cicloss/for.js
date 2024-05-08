8185d2eeaea52e9a81f7380d58c4b6c3a97f57d0
Gabi13st.h <gabs13.h@gmail.com>
2024-05-07 23:57:49 -0400

    Agrega ciclo for in

--- assets/javascript/cicloss/for.js
+++ assets/javascript/cicloss/for.js
@@ -4,3 +4,8 @@ console.warn('For tradicional');
 for( let i = 0; i < heroes.length; i++ ) {
     console.log( heroes[i] );
 }
+
+console.warn('For in');
+for( let i in heroes ) {
+    console.log( heroes[i] );
+}
