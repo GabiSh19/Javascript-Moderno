e23c838ae816ed3e107eaa43e3d3afe2823b0f4e
Gabi13st.h <gabs13.h@gmail.com>
2024-05-07 23:55:43 -0400

    Reestructura archivos

--- assets/javascript/cicloss/estructuras-de-control.js
+++ assets/javascript/cicloss/estructuras-de-control.js
@@ -1,4 +1,4 @@
 // ESTRUCTURAS DE CONTROL DE FLUJO  
 // Veremos: if - else if - ciclo while, for, do while, 
 // for in for of... 
-// Operador ternario: (condicion) ? valor1(si es verdadero) : valor2 (si es falso)
+// Operador ternario: (condicion) ? valor1(si es verdadero) : valor2 (si es falso)gt
