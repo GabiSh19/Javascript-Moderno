392402fb3b0579803e53ef363751928535894942
Gabi13st.h <gabs13.h@gmail.com>
2024-05-08 23:45:11 -0400

    Comenta documento if-else

--- assets/javascript/bases/if-else.js
+++ assets/javascript/bases/if-else.js
@@ -1,27 +1,24 @@
-
-
+// IF ELSE, ELSE IF 
 let a = 5;
-
 
 if ( a >= 10 ) { // undefined, null, una asignación
     console.log('A es mayor o igual a 10');
 } else {
     console.log('A es menor a 10');
 }
-
-
 // console.log('Fin de programa');
 
+// Nueva instancia del objeto date
 const hoy = new Date(); // {}
 let dia = hoy.getDay(); // 0: Domingo, 1: lunes, 2: martes.....
 
 console.log({ dia });
 
+//Else if
 if ( dia === 0 ) {
     console.log('Domingo');
 } else if( dia === 1 ) {
     console.log('Lunes');
-    
     // if ( dia === 1 ) {
     //     console.log('Lunes');
     // } else {
@@ -32,7 +29,6 @@ if ( dia === 0 ) {
 } else {
     console.log('No es lunes, martes o domingo...');
 }
-
 
 // Sin usar If Else, o Switch, únicamente objetos
 dia = 6; // 0:domingo, 1: lunes....
