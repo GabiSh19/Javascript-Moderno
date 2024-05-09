3ab213b73e6ec630935d3689ba9ce97adbb078d8
Gabi13st.h <gabs13.h@gmail.com>
2024-05-08 22:50:29 -0400

    documenta archivo valor-referencia

--- assets/javascript/bases/valor-referencia.js
+++ assets/javascript/bases/valor-referencia.js
@@ -1,11 +1,15 @@
+// Paso de variables por valor y por referencia: 
+
+// Acá estamos pasando variables por valor ( valores primitivos) 
 let a = 10;
 let b = a;
 a = 30;
 
 console.log({ a, b });
 
-
+// Cuando se trata de objetos estamos pasando variables por referencia, para no afectar el objeto original usamos el ...SPREAD
 let juan = { nombre: 'Juan' };
+// ... = SPREAD:  Operador especial para separar todas las propiedades y valores de un objeto. 
 let ana  = { ...juan };
 ana.nombre = 'Ana';
 
