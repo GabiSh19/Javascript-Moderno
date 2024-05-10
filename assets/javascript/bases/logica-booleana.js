ba1853e6d095e1170cb8de444a7adcf054369cda
Gabi13st.h <gabs13.h@gmail.com>
2024-05-09 23:03:47 -0400

    Agrega programa de asignaciones en Javascript

--- assets/javascript/bases/logica-booleana.js
+++ assets/javascript/bases/logica-booleana.js
@@ -24,11 +24,11 @@ console.log( true && true ); // true
 console.log( true && !false ); // true
 
 console.log('=========');
-console.log(  regresaFalse() && regresaTrue() ); // false
+console.log(  regresaFalse() && regresaTrue() ); // false, ejecuta el false y se sale. No evalúa toda la línea.
 console.log(  regresaTrue() && regresaFalse() ); // false
 
 console.log('==== && =====');
-regresaFalse() && regresaTrue();
+regresaFalse() && regresaTrue(); // aparece el mensaje de cada función. 
 
 console.log( '4 condiciones ', true && true && true && false ); // false
 
@@ -40,3 +40,27 @@ console.log( false || false );
 
 console.log( regresaTrue() || regresaFalse() );
 console.log( '4 condiciones ', true || true || true || false ); // true
+
+
+// ASIGNACIONES
+
+console.warn('Asignaciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = false && 'Hola Mundo' && 150; // ?
+const a2 = 'Hola' && 'Mundo' && soyFalso && true;
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo' || true;
+
+console.log({a1, a2, a3, a4, a5 });
+
+if ( regresaFalse() && regresaTrue() && (true || false || true) ) {
+    console.log('Hacer algo');
+} else {
+    console.log('Hacer otra cosa');
+}
+
