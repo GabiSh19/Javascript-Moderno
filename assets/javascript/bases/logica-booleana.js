4dca9aa5ebd8e149f5ab5b6f8bfe3a124b0b9cf9
Gabi13st.h <gabs13.h@gmail.com>
2024-05-09 22:49:03 -0400

    Agrega operador AND al archivo lógica booleana

--- assets/javascript/bases/logica-booleana.js
+++ assets/javascript/bases/logica-booleana.js
@@ -8,9 +8,21 @@ const regresaFalse = () => {
     return false;
 }
 
+// NEGACIÓN 
+
 console.warn('Not o la negación');
 console.log( true ); // true
 console.log( !true ); // false
 console.log( !false ); // true
 
 console.log( !regresaFalse() ); // true
+
+//OPERADOR AND
+
+console.warn('And'); // true si todos los valores son verdaderos
+console.log( true && true ); // true
+console.log( true && !false ); // true
+
+console.log('=========');
+console.log(  regresaFalse() && regresaTrue() ); // false
+console.log(  regresaTrue() && regresaFalse() ); // false
