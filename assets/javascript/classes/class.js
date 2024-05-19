089f6c3126b2924e365487cc1c8f5df101d4607d
Gabi13st.h <gabs13.h@gmail.com>
2024-05-18 22:27:40 -0400

    Agrega funcionalidad a documento clases

--- assets/javascript/classes/class.js
+++ assets/javascript/classes/class.js
@@ -4,14 +4,29 @@ class Persona {
     frase;
 
     constructor(nombre, codigo, frase) {
+
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
     
     }
 
+    quienSoy(){
+        console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo}`)
+    }
+
+    miFrase() {
+        this.quienSoy(); 
+        console.log(` ${ this.codigo } dice: ${ this.frase } `)
+    }
+
 }
 
 
 const spiderman = new Persona ('Peter Parker', 'Spidy', 'Hola, soy Spiderman')
-console.log(spiderman)
+const ironman = new Persona ('Tony Stark', 'IronMan', 'Yo soy Ironman')
+console.log( spiderman ); 
+console.log( ironman );
+
+spiderman.miFrase();
+ironman.miFrase();
