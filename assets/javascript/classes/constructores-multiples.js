01c2871b54a4f029d2280c11a9b885b991c972da
Gabi13st.h <gabs13.h@gmail.com>
2024-05-20 11:48:16 -0400

    Agrega lógica a documento constructores múltiples

--- assets/javascript/classes/constructores-multiples.js
+++ assets/javascript/classes/constructores-multiples.js
@@ -1,5 +1,9 @@
 class Persona {
 
+    static porObjeto ({ nombre, apellido, pais }) {
+        return new Persona ( nombre, apellido, pais );    
+    }
+
     constructor ( nombre, apellido, pais ) {
         
         this.nombre = nombre;
@@ -12,5 +16,22 @@ class Persona {
         console.log(`Información: ${this.nombre},  ${this.apellido},  ${this.pais}`);
     }
 
+}
+
+const nombre1 = 'Melissa', 
+    apellido1 = 'Flores', 
+    pais1 = 'Colombia';
+
+const fher = {
+
+    nombre: 'Fher',
+    apellido: 'Herrera',
+    pais: 'Costa Rica'
 
 }
+
+const persona1 = new Persona ( nombre1, apellido1, pais1 );
+const persona2 = Persona.porObjeto( fher );
+
+persona1.getInfo();
+persona2.getInfo();
